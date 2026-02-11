6c6f69f837847d928386ded901e5a898c49615df
Tobias Eppenschwandtner <is231014@ustp-students.at>
2026-02-11 16:15:24 +0100

    Added file read functionality - still in testing; likely choosing Read.Bytes

--- glyphcheck.go
+++ glyphcheck.go
@@ -1,9 +1,52 @@
 package main
 
 import "fmt"
+import "os"
+import "log"
+import "bufio"
+import "io"
 
 func main() {
 
-fmt.Println("Hello World!");
+var fileName string = "glyphcheck.go"
+var file *os.File;
+var errOpenFile error;
+
+file, errOpenFile = os.Open(fileName);
+if errOpenFile != nil {
+
+	log.Fatal(errOpenFile);
+
+}
+defer file.Close();
+
+var reader *bufio.Reader;
+reader = bufio.NewReader(file);
+
+var test []byte;
+var testString string;
+var testByteReadError error;
+
+for {
+
+	test, testByteReadError = reader.ReadBytes('\n');
+
+	if testByteReadError != nil && testByteReadError != io.EOF {
+
+		log.Fatal(testByteReadError);
+
+	}
+
+	if testByteReadError == io.EOF && len(test) == 0 {
+
+		break
+
+	}
+
+	testString = string(test);
+
+	fmt.Print(testString);
+
+}
 
 }
