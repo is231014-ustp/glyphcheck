3c1d0178b9649b350af6af838998af5c813e930c
Tobias Eppenschwandtner <is231014@ustp-students.at>
2026-02-17 21:02:17 +0100

    glyphcheck is now using a configuration file; dev rant: took way longer then i thought, needed to change a lot... was a long day

--- glyphcheck.go
+++ glyphcheck.go
@@ -8,12 +8,14 @@ import "fmt"
 import "os"
 import "bufio"
 import "io"
-import "golang.org/x/text/unicode/norm"
 import "unicode/utf8"
 import "unicode"
 import "strings"
 import "io/fs"
 import "path/filepath"
+
+import "golang.org/x/text/unicode/norm"
+import "gopkg.in/yaml.v3"
 
 type allowList struct {
 
@@ -23,55 +25,56 @@ type allowList struct {
 
 }
 
-var excludedDirectories = map[string]struct{}{
+type Config struct {
 
-	".git":{},
-
-}
-
-var allowedExtensions = map[string]struct{}{
-
-	".go":{},
-
-}
-
-var devAllowList = allowList{
-
-	Scripts: []*unicode.RangeTable{
-		
-		unicode.Latin,
-		unicode.Common,
-
-	},
-
-	Categories: []*unicode.RangeTable{
-
-		unicode.Letter,
-		unicode.Number,
-		unicode.Punct,
-
-	},
-
-	Characters: map[rune]struct{}{
-
-		'\n':{},
-		'\r':{},
-		'\t':{},
-		' ':{},
-		'=':{},
-		'+':{},
-		'<':{},
-		'>':{},
-
-	},
+	Root string `yaml:"root"`
+	AllowedExtensions []string `yaml:"allowed_extensions"`
+	ExcludedDirectories []string `yaml:"excluded_directories"`
+	Allowed struct {
+		Scripts []string `yaml:"scripts"`
+		Categories []string `yaml:"categories"`	
+		Characters []string `yaml:"characters"`
+	} `yaml:"allowed"`
 
 }
 
 func main() {
 
-	var root string = ".";
+	var configName string = ".glyphcheck.yaml";
 
-	if scanDirectory(root) {
+	var currentConfig Config;
+	var currentConfigLoadError error;
+
+	currentConfig, currentConfigLoadError = loadConfig(configName);
+
+	if currentConfigLoadError != nil {
+
+		fmt.Println(currentConfigLoadError);
+		os.Exit(1);
+
+	}
+
+	var currentConfigInvalid error;
+
+	currentConfigInvalid = validateConfig(currentConfig);
+
+	if currentConfigInvalid != nil {
+
+		fmt.Print(currentConfigInvalid);
+		os.Exit(1);
+
+	}
+
+	var excludedDirectories map[string]struct{};
+	excludedDirectories = directoryStringSliceToMap(currentConfig.ExcludedDirectories);
+	
+	var allowedExtensions map[string]struct{};
+	allowedExtensions = extensionStringSliceToMap(currentConfig.AllowedExtensions);
+
+	var currentAllowList allowList;
+	currentAllowList = buildAllowList(currentConfig);
+
+	if scanDirectory(currentConfig.Root, currentAllowList, excludedDirectories, allowedExtensions) {
 	
 		os.Exit(1)
 
@@ -167,7 +170,7 @@ func checkLine(readBytes []byte, lineIndex int, fileName string, currentAllowLis
 		if !runeIsAllowed {
 
 			violationInLine = true;	
-			fmt.Printf("File: %s; Line: %d; Column: %d; Byte Offset: %d; Character: %c; Unicode: %U\n",fileName, lineIndex, runeIndex + 1, i, currentRune, currentRune);
+			fmt.Printf("File: %s; Line: %d; Column: %d; disallowed unicode character: %U (%c)\n",fileName, lineIndex, runeIndex + 1, currentRune, currentRune);
 
 		}
 	
@@ -178,12 +181,6 @@ func checkLine(readBytes []byte, lineIndex int, fileName string, currentAllowLis
 	}
 
 	return violationInLine;
-
-}
-
-func getAllowList() allowList {
-
-	return devAllowList;
 
 }
 
@@ -214,11 +211,7 @@ func isAllowed(runeToCheck rune, currentAllowList allowList) bool {
 
 }
 
-func scanDirectory (root string) bool {
-
-	var currentAllowList allowList;
-	currentAllowList = getAllowList();
-
+func scanDirectory (root string, currentAllowList allowList, excludedDirectories map[string]struct{}, allowedExtensions map[string]struct{}) bool {
 
 	var violationInDirectory bool = false;
 
@@ -292,4 +285,246 @@ func scanDirectory (root string) bool {
 	return violationInDirectory
 
 
+}
+
+func loadConfig(configPath string) (Config, error) {
+
+	var currentConfig Config;
+
+	currentConfig.Root = ".";
+	currentConfig.AllowedExtensions = []string{".go"};
+	currentConfig.ExcludedDirectories = []string{".git"}
+
+	currentConfig.Allowed.Scripts = []string{"Latin", "Common"};
+	currentConfig.Allowed.Categories = []string{"Letter", "Number", "Punct"}
+	currentConfig.Allowed.Characters = []string{"\n","\r","\t", " ", "=", "+", "-", "<", ">"}
+
+	var configFileData []byte;
+	var configFileError error;
+
+	configFileData, configFileError = os.ReadFile(configPath);
+
+	if configFileError != nil {
+
+		return currentConfig, configFileError;
+
+	}
+
+	var yamlConfigFileError error;
+
+	yamlConfigFileError = yaml.Unmarshal(configFileData, &currentConfig);
+
+	if yamlConfigFileError != nil {
+
+		return currentConfig, yamlConfigFileError;
+
+	}
+
+
+	return currentConfig, nil;
+
+}
+
+func validateConfig(configToCheck Config) error {
+
+	if strings.TrimSpace(configToCheck.Root) == "" {
+
+		return fmt.Errorf("root can not bee empty\n");
+
+	}
+
+	if len(configToCheck.AllowedExtensions) == 0 {
+
+		return fmt.Errorf("extensions must contain at least one entry\n");
+
+	}
+
+	if len(configToCheck.Allowed.Scripts) == 0 {
+
+		return fmt.Errorf("allowed.scripts can not be empty\n");
+
+	}
+
+	if len(configToCheck.Allowed.Categories) == 0 {
+
+		return fmt.Errorf("allowed.categories can not be emtpy\n");
+
+	}
+
+	return nil;
+
+}
+
+func directoryStringSliceToMap (directories []string) (map[string]struct{}) {
+
+	var directoryMap map[string]struct{};
+	directoryMap = make(map[string]struct{});
+
+	var directoryString string;
+
+	for _, directoryString = range directories {
+
+		directoryString = strings.TrimSpace(directoryString);
+
+		if directoryString == "" {
+
+			continue;
+
+		}
+
+		directoryMap[directoryString] = struct {}{};
+
+	}
+
+	return directoryMap;
+
+}
+
+func extensionStringSliceToMap (extensions []string) (map[string]struct{}) {
+
+	var extensionMap map[string]struct{};
+	extensionMap = make(map[string]struct{});
+
+	var extensionString string;
+
+	for _, extensionString = range extensions {
+
+		extensionString = normalizeExtension(extensionString);
+
+		if extensionString == "" {
+
+			continue;
+
+		}
+
+		extensionMap[extensionString] = struct {}{};
+
+	}
+
+	return extensionMap;
+
+}
+
+func normalizeExtension (extension string) string {
+
+	extension = strings.ToLower(strings.TrimSpace(extension));
+
+	if extension == "" {
+
+		return "";
+
+	}
+
+	if !strings.HasPrefix(extension, ".") {
+
+		extension = "." + extension
+
+	}
+
+	return extension;
+
+}
+
+func buildAllowList (config Config) allowList {
+
+	var currentAllowList allowList;
+	
+	currentAllowList.Characters = make(map[rune]struct{});
+	currentAllowList.Characters = characterStringSliceToMap(config.Allowed.Characters);
+
+	var scriptName string;
+
+	for _, scriptName = range config.Allowed.Scripts {
+
+		var scriptRangeTable *unicode.RangeTable;
+		scriptRangeTable = scriptNameToRangeTable(scriptName);
+
+		if scriptRangeTable == nil {
+
+			fmt.Printf("Unkown script in config: %c\n", scriptName);
+			continue;
+
+		}
+
+		currentAllowList.Scripts = append(currentAllowList.Scripts, scriptRangeTable);
+
+	}
+
+	var categoryName string;
+	
+	for _, categoryName = range config.Allowed.Categories {
+
+		var categoryRangeTable *unicode.RangeTable;
+		categoryRangeTable = categoryNameToRangeTable(categoryName);
+
+		if categoryRangeTable == nil {
+
+			fmt.Printf("Unkown Category in config: %c\n", categoryName);
+			continue;
+		}
+	
+		currentAllowList.Categories = append(currentAllowList.Categories, categoryRangeTable);
+
+	}
+
+
+	return currentAllowList;
+
+}
+
+func characterStringSliceToMap (characters []string) (map[rune]struct{}) {
+
+	var characterMap map[rune]struct{};
+	characterMap = make(map[rune]struct{});
+
+	var characterString string;
+
+	for _, characterString = range characters {
+
+		var characterRune rune;
+
+		for _, characterRune = range characterString {
+
+			characterMap[characterRune] = struct{}{};
+
+		}
+
+	}
+
+	return characterMap;
+
+}
+
+func categoryNameToRangeTable(categoryName string) *unicode.RangeTable {
+
+	categoryName = strings.TrimSpace(categoryName);
+
+	switch categoryName {
+
+	case "Letter":
+		return unicode.Letter;
+	case "Number":
+		return unicode.Number;
+	case "Punct":
+		return unicode.Punct;
+
+	}
+
+	return nil
+}
+
+func scriptNameToRangeTable(scriptName string) *unicode.RangeTable {
+
+	scriptName = strings.TrimSpace(scriptName);	
+
+	switch scriptName {
+
+	case "Latin":
+		return unicode.Latin;
+	case "Common":
+		return unicode.Common;
+
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module glyphcheck/glyphcheck
 
 go 1.24.0
 
-require golang.org/x/text v0.34.0
+require (
+	golang.org/x/text v0.34.0
+	gopkg.in/yaml.v3 v3.0.1
+)
